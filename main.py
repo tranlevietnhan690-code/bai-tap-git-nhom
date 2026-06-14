13d57cf1f14b0680b4837858c63c41f7de70de8a
tranlevietnhan690-code <tranlevietnhan690@gmail.com>
2026-06-14 18:26:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def tinh_thue_thu_nhap(thu_nhap):
-    # Kết hợp logic: Giảm trừ 4 triệu của A và tính thuế 10% của B
-    thu_nhap_tinh_thue = thu_nhap - 4000000
-    return max(0, thu_nhap_tinh_thue * 0.1)
-
-print("Thue cua ban phai nop la:", tinh_thue_thu_nhap(10000000))
+def tinh_diem_gpa(diem_so):
+    return0.0
+print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))    
