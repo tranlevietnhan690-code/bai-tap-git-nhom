fe61e19caea299186844a261f06cd6619ec3b7d7
TrangThach2503 <trangthach2503@gmail.com>
2026-06-14 18:40:39 +0700

    feat: dev B cap nhat code gpa thuc te

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 def tinh_diem_gpa(diem_so):
-    return0.0
-print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))    
+    if diem_so >= 8.5: return 4.0
+    elif diem_so >= 7.0: return 3.0
+    else: return 2.0git 
