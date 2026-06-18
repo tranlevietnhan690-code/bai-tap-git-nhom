7a300e231d5ead94e58547d62026a8d8b58672f9
Cao Dinh Nam <caodinhnam782005@gmail.com>
2026-06-18 21:36:15 +0700

    feat: tối ưu lại công thức tính gpa

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
         return 4.0 # Code của Dev B
     else:
-        return round((diem_so / 10) * 4, 2) # Code của Dev A
+        return (diem_so / 10) * 3.5 # Sửa sai từ hệ số 4 thành 3.5!
